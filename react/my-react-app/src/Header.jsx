b1ba8829502f43535d74f4070e4a710fe31aab8e
Shin Han <kandarata@gmail.com>
2024-06-10 02:21:44 +0000

    react-project

---
+++ react/my-react-app/src/Header.jsx
@@ -0,0 +1,12 @@
+import Header from './Heaer.jsx';
+
+function HeaderJ() {
+    
+    return(
+        <heaer>
+            <h1>My website</h1>
+        </heaer>
+    );
+}
+
+export default Header;
